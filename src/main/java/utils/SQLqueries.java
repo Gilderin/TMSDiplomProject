d57b84a93925952c80ba9fba0777a973074f0acb
Balexer <balexer@mail.ru>
2020-08-21 19:32:32 +0300

    refactor SQLqueries

--- src/main/java/utils/SQLqueries.java
+++ src/main/java/utils/SQLqueries.java
@@ -4,6 +4,7 @@ public class SQLqueries {
 
     private String ProjectInformation = "";
     private String LoginInformation = "";
+    private String TestCasesInformation = "";
 
     public String ProjectInformationSelect(Integer id) {
         ProjectInformation ="Select * from public.\"Projects\" as pr\n" +
@@ -16,6 +17,12 @@ public class SQLqueries {
         LoginInformation = "Select * from public.\"Users\"\n" +
                 "where \"Users\".\"id\"='"+id+"'";
         return LoginInformation;
+    }
+
+    public String TestCasesInformationSelect(Integer id) {
+        TestCasesInformation = "Select * from public.\"TestCases\"\n" +
+                "where \"TestCases\".\"id\"='"+id+"'";
+        return TestCasesInformation;
     }
 
 }
